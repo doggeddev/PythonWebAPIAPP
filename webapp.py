a2b5da4a8b989984100e4451ae893a260f45b724
rjdrexel <richdrexel@gmail.com>
2017-02-12 01:57:55 -0500

    Refactored some code, general cleanup

--- webapp.py
+++ webapp.py
@@ -9,38 +9,33 @@ import requests
    calling the GET /weather endpoint, or by navigating to a web page and inputting in the information.
 
     This program is simply for practice and learning more about APIs, routing, and HTTP Requests/Responses.
-
 """
 
-
+# TODO: Break up code into separate modules based on the responsibility
 app = Flask(__name__)
 
 
-#API Endpoint 
+# API Endpoint
 @app.route('/weather', methods=['GET'])
 def apiweather():
-
-    homeJsonObject = None
-    workJsonObject = None
-
+    home_json_object = None
+    work_json_object = None
 
     for param in request.args:
         if param == 'home':
-            homeJsonObject = check_home_location(request.args['home'])
+            home_json_object = check_home_location(request.args['home'])
         if param == 'work':
-            workJsonObject = check_work_location(request.args['work'])
+            work_json_object = check_work_location(request.args['work'])
 
-    return determine_return_values(homeJsonObject, workJsonObject)
+    return determine_return_values(home_json_object, work_json_object)
 
 
-#This receives information from the web page making the request
+# This receives information from the web page making the request
 @app.route('/weather', methods=['POST'])
 def weather():
-
     home = ''
     work = ''
 
-    home = request.form['home']
     home = request.form['home']
     work = request.form['work']
 
@@ -49,63 +44,61 @@ def weather():
 
     template = determine_return_values(home_json_object, work_json_object)
 
-    return  template
+    return template
 
 
+# TODO: be sure to check for more inputs than can cause failures... ie alphanumeric... etc
 def check_home_location(home):
-    homeobject = None
+    home_json_object = None
 
     if home is not '':
-        homecoords = home.split(',')
+        home_coords = home.split(',')
+        lat = str.strip(home_coords[0])  # spaces must be removed, otherwise OpenWeather returns an error
+        lon = str.strip(home_coords[1])
 
-        lat = str.strip(homecoords[0])
-        lon = str.strip(homecoords[1])
-
-        rlist0 = requests.get(
-            'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=2350adbb3165e0ae4f996481c837eb8b')
-        homeobject = rlist0.json()
-
-    return homeobject
+    return get_weather_conditions(lat, lon)
 
 
+# TODO: be sure to check for more inputs than can cause failures... ie alphanumeric... etc
 def check_work_location(work):
-    workobject = None
+    work_json_object = None
 
     if work is not '':
-        workcoords = work.split(',')
+        work_coords = work.split(',')
+        lat = str.strip(work_coords[0])
+        lon = str.strip(work_coords[1])
 
-        lat = str.strip(workcoords[0])
-        lon = str.strip(workcoords[1])
+    return get_weather_conditions(lat, lon)
 
 
+def get_weather_conditions(lat, lon):
+    api_response = requests.get(
+        'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=2350adbb3165e0ae4f996481c837eb8b')
 
-        rlist1 = requests.get(
-            'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=2350adbb3165e0ae4f996481c837eb8b')
-        workobject = rlist1.json()
+    return api_response.json()
 
-    return workobject
 
 def determine_return_values(home_json_object, work_json_object):
-
     template = None
 
     if home_json_object is None and work_json_object is None:
-        template = render_template('conditonspage.html', home ='', work ='')
+        template = render_template('conditonspage.html', home='', work='')
         return template
 
     if home_json_object == work_json_object:
-         template = render_template('conditonspage.html', home=home_json_object, work='')
-         return template
+        template = render_template('conditonspage.html', home=home_json_object, work='')
+        return template
 
     if work_json_object is None:
-         template = render_template('conditonspage.html', home=home_json_object, work='')
-         return template
+        template = render_template('conditonspage.html', home=home_json_object, work='')
+        return template
 
     if home_json_object is None:
         template = render_template('conditonspage.html', home='', work=work_json_object)
         return template
 
-    return  render_template('conditonspage.html', home=home_json_object, work=work_json_object)
+    return render_template('conditonspage.html', home=home_json_object, work=work_json_object)
+
 
 @app.route('/')
 def index():
